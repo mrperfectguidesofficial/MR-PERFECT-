44ddeabea90ea4ce3724edfbf6365915d8fe04ad
mrperfectguidesofficial <allxworld.in@gmail.com>
2026-05-08 08:49:03 +0530

    Delete API directory

--- API/cors.js
+++
@@ -1,7 +0,0 @@
-const cors = require('cors');
-
-module.exports = cors({
-    origin: process.env.ALLOWED_ORIGIN || '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders:['Content-Type', 'Authorization']
-});
