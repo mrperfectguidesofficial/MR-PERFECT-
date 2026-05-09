767bb8a0902bef995d9707ee73cbde80051cdeb9
mrperfectguidesofficial <allxworld.in@gmail.com>
2026-05-10 01:09:36 +0530

    Refactor CORS configuration to use allowed origins

--- api/cors.js
+++ api/cors.js
@@ -1,9 +1,23 @@
 const cors = require('cors');
 
+const allowedOrigins = process.env.ALLOWED_ORIGIN
+  ? process.env.ALLOWED_ORIGIN
+      .split(',')
+      .map(origin => origin.trim())   // 🔥 space remove
+  : [];
+
 const corsOptions = {
-    origin: process.env.ALLOWED_ORIGIN || '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders:['Content-Type', 'Authorization']
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 module.exports = cors(corsOptions);
